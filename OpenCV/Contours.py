c2bb5e5dad815eeee813b18911901696f746fa8e
yangqingter <18930475858@189.cn>
2022-06-14 21:57:27 +0800

    add

--- OpenCV/Contours.py
+++ OpenCV/Contours.py
@@ -19,21 +19,13 @@ im_show("b",thresh)
 
 img_Blur= cv2.GaussianBlur(thresh, (3,3),1)
 
-contours, hierarchy = cv2.findContours(img_Blur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+edg = cv2.Canny(img_Blur,100,200)
+
+#contours, hierarchy = cv2.findContours(img_Blur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 img_c = img.copy()
-im_show("c",img_c)
-t1= (hierarchy[:,:,2]==-1)[0]
+im_show("c",edg)
 
-cv2.drawContours(img_c, contours[2:], -1, (0,0,255), 1)
-im_show("c",img_c)
-plt_show(img_c)
-
-
-print(t1)
-
-area = cv2.contourArea(contours[3])
-print(area)
 
 
 
